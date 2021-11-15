e4502da2ff858b2393851beeb4fca4f94cb6230d
PHAMCANHDINH <dinhphamcanh@gmail.com>
2021-11-15 23:37:45 +0700

    first commit

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,60 @@
+const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+
+
+const getAllTasks = asyncWrapper(async (req,res) =>{
+        const tasks= await Task.find({})
+        res.status(200).json({tasks,amount:tasks.length})
+})
+
+const createTask = asyncWrapper(async(req,res) => {
+        const task= await Task.create(req.body)
+        res.status(201).json({task})
+})
+
+const getTask = asyncWrapper(async(req,res,next) => {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+         if(!task){
+             const err = new Error('Not Found')
+             err.status = 404
+             return next(err)
+             return res.status(404).json(`no match ${taskID}`)
+         }
+        res.status(200).json({task})
+    
+})
+
+const updateTask = asyncWrapper(async(req,res) => {
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true,
+        })
+        res.status(200).json({tasks})
+        if(!task){
+            return res.status(404).json(`no match ${taskID}`)
+        }
+})
+
+const deleteTask = asyncWrapper(async(req,res) => {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json(`no match ${taskID}`)
+
+        }
+        res.status(200).json({})  
+    res.send('delete task')
+})
+
+
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
